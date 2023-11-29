548a8ae677b1c02bc8821a9cd2b8aa5803131fb9
Mariam Joglidze <mariamjoglidze1@gmail.com>
2023-11-29 18:20:15 +0400

    create and configure showWeatherButton

--- CollaborationTeamOne/Scenes/ WeatherPage/WeatherController.swift
+++ CollaborationTeamOne/Scenes/ WeatherPage/WeatherController.swift
@@ -29,6 +29,11 @@ final class WeatherController: UIViewController {
         return stackView
     }()
     
+    let showWeatherButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
     // MARK: - lifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,18 +65,31 @@ private extension WeatherController {
         stackView.spacing = 10
         stackView.distribution = .fillEqually
         
+        showWeatherButton.setTitle("Show Weather", for: .normal)
+        showWeatherButton.backgroundColor = .lightGray
+        showWeatherButton.layer.cornerRadius = 23
         
     }
     
     func layout() {
         view.addSubview(stackView)
+        view.addSubview(showWeatherButton)
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        showWeatherButton.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
             stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
             stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            stackView.heightAnchor.constraint(equalToConstant: 100)
+            stackView.heightAnchor.constraint(equalToConstant: 100),
+            
+            showWeatherButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 30),
+            showWeatherButton.widthAnchor.constraint(equalToConstant: 200),
+            showWeatherButton.heightAnchor.constraint(equalToConstant: 50),
+            showWeatherButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            
+
 
         ])
         
