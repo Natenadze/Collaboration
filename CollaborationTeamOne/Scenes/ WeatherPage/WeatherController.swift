343024a2e68e9b100f1dd67a5a63547d477ec0e4
Mariam Joglidze <mariamjoglidze1@gmail.com>
2023-11-29 17:43:16 +0400

    add textFields

--- CollaborationTeamOne/Scenes/ WeatherPage/WeatherController.swift
+++ CollaborationTeamOne/Scenes/ WeatherPage/WeatherController.swift
@@ -14,7 +14,20 @@ final class WeatherController: UIViewController {
     
     
     // MARK: - UI Elements
+    let latTextField: UITextField = {
+        let latTextField = UITextField()
+        return latTextField
+    }()
     
+    let lonTextField: UITextField = {
+        let lonTextField = UITextField()
+        return lonTextField
+    }()
+    
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        return stackView
+    }()
     
     // MARK: - lifeCycle
     override func viewDidLoad() {
@@ -34,25 +47,41 @@ private extension WeatherController {
     
     
     func style() {
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        latTextField.borderStyle = .roundedRect
+        latTextField.placeholder = "Text field 1"
+        
+        lonTextField.borderStyle = .roundedRect
+        lonTextField.placeholder = "Text field 2"
+        
+        stackView.addArrangedSubview(latTextField)
+        stackView.addArrangedSubview(lonTextField)
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        
         
     }
     
     func layout() {
-        
-        
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
         NSLayoutConstraint.activate([
-            
-            
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            stackView.heightAnchor.constraint(equalToConstant: 100)
+
         ])
         
     }
 }
 
 
-/*
- // MARK: - Preview
- #Preview {
- WeatherController()
- }
- */
+
+// // MARK: - Preview
+// #Preview {
+// WeatherController()
+// }
+
