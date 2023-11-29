4ffc458f574284885dea75c89f47435ea0c1e04e
davit <davit.natenadze@gmail.com>
2023-11-29 10:19:51 +0100

    Initial Commit

---
+++ CollaborationTeamOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollaborationTeamOne
+//
+//  Created by Davit Natenadze on 29.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
