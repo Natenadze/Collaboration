24b52ae8800654179525b106953ec01d197afd8d
NanaJimsheleishvili <jimsheleishvili.nana@tbcacademy.ge>
2023-12-01 22:47:29 +0400

    Revert "Merge pull request #5 from Natenadze/Salome"
    This reverts commit 381cea261661898943b33f00be2ddec959e8c996, reversing
    changes made to 164b2f4b487b0cb276d121df8c8370d6bc963e61.

---
+++ CollaborationTeamOne/Scenes/Salome/SolarResourceController.swift
@@ -0,0 +1,57 @@
+//
+//  SolarResourceController.swift
+//  CollaborationTeamOne
+//
+//  Created by Davit Natenadze on 29.11.23.
+//
+
+import UIKit
+
+
+final class SolarResourceController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - UI Elements
+    
+    
+    // MARK: - lifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+    
+    // MARK: - Methods
+    
+}
+
+
+// MARK: - Style & Layout
+
+private extension SolarResourceController {
+    
+    
+    func style() {
+        view.backgroundColor = .cyan
+        
+    }
+    
+    func layout() {
+        
+        
+        NSLayoutConstraint.activate([
+            
+            
+        ])
+        
+    }
+}
+
+/*
+ // MARK: - Preview
+ #Preview {
+ SolarResourceController()
+ }
+ */
